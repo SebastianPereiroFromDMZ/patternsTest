4c1a2d33d23df8ec0d8aa89facf1e525494a7f2a
Kost Kuzmin <Kuzmin_konstantin@bk.ru>
2023-12-08 17:12:28 +0300

    commit_4

--- src/main/java/creational/abstract_factory/start/Start.java
+++ src/main/java/creational/abstract_factory/start/Start.java
@@ -1,4 +1,18 @@
 package creational.abstract_factory.start;
 
+import creational.abstract_factory.factory.impl.RussianFactory;
+import creational.abstract_factory.factory.impl.USAFactory;
+import creational.abstract_factory.factory.interfaces.TransportFactory;
+import creational.abstract_factory.transport.interfaces.Aircraft;
+
 public class Start {
+    public static void main(String[] args) {
+        TransportFactory russianFactory = new RussianFactory();
+        TransportFactory usaFactory = new USAFactory();
+        Aircraft tu134 = russianFactory.createAircraft();
+        tu134.flight();
+
+        Aircraft boeing747 = usaFactory.createAircraft();
+        boeing747.flight();
+    }
 }
